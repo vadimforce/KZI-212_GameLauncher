30954d0d2d9d45a1cdac54617615e9e4c26f705f
Denis <92718939+DenisM2001@users.noreply.github.com>
2021-11-06 16:10:01 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using сапёр.Controllers;
+
+namespace сапёр
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MapControllers.Init(this);
+
+        
+        }
+    }
+}
